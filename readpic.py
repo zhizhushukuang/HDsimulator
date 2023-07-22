51f31694d38cdc7282d971f276e638af70d860c0
zhizhushukuang <84705144+zhizhushukuang@users.noreply.github.com>
2023-07-22 18:28:55 +0800

    v1.1
    外置了一堆东西
    总之现在可以连续读图了

--- readpic.py
+++ readpic.py
@@ -9,7 +9,7 @@ from pygame.draw import circle
 from pygame.locals import *
 
 ConfigPath = os.getcwd() + "\picconfig.json"
-PicPath = os.getcwd() + "\IMGNEXT.png"
+#PicPath = os.getcwd() + "\IMGNEXT.png"
 IMGPATH=os.getcwd() + "\IMG.png"
 UsedColorQueue = []
 WalkedPath = []
@@ -17,6 +17,8 @@ WalkedPath = []
 Quad=60
 WIDTH = 9 * Quad
 HEIGHT = 12 * Quad
+# 定义全局变量记录当前图像索引
+current_img_index = 0
 
 # 颜色定义
 RED = pygame.Color(255, 0, 0)
@@ -38,83 +40,217 @@ def ChangeQuadColor(Position, SquareColor, NewColor):
     y = int(Position[1] / Quad)
     SquareColor[y][x] = NewColor
 
+import cv2
+import json
+import os
+import numpy as np
+from math import pi
+
+def evaluate_color_around_pixel(image, x, y, window_size=5, max_threshold=255*0.8):
+    """
+    计算某个像素点周围区域的颜色评价值，排除RGB值高于阈值的像素点
+    参数：
+    image: 输入图像
+    x, y: 像素点的坐标
+    window_size: 窗口大小，即在(x, y)周围取的区域大小
+    max_threshold: RGB值的最大阈值，高于该值的像素点将被忽略
+    返回：
+    color_value: 像素点周围区域的颜色评价值，可以是颜色的平均值或其他统计量
+    """
+    h, w, _ = image.shape
+    half_window = window_size // 2
+
+    # 确保窗口不超出图像边界
+    x_start = max(x - half_window, 0)
+    x_end = min(x + half_window, w - 1)
+    y_start = max(y - half_window, 0)
+    y_end = min(y + half_window, h - 1)
+
+    # 提取像素点周围区域的颜色值
+    region_colors = image[y_start:y_end+1, x_start:x_end+1]
+
+    # 排除RGB值高于阈值的像素点
+    region_colors = region_colors[np.all(region_colors <= max_threshold, axis=-1)]
+
+    # 如果周围没有符合条件的像素点，则返回None
+    if len(region_colors) == 0:
+        return None
+
+    # 计算颜色评价值，这里简单地使用颜色的平均值作为评价值
+    color_value = np.mean(region_colors, axis=(0,))
+
+    return color_value
+
+def read_pic_set(path):
+    try:
+        with open(path, 'r') as f:
+            data = json.load(f)
+            init_point_ratio = data["init_point_ratio"]
+            step_ratio = data["step_ratio"]
+            xfixset=data["xfixset"]
+            yfixset=data["yfixset"]
+            fixlinestartpoint=data["fixlinestartpoint"]
+            picturename=data["picturename"]
+            pictureformat=data["picture format"]
+    except:
+            init_point_ratio = [0.415, 0.09]
+            step_ratio = 0.05
+            xfixset=1
+            yfixset=1
+            fixlinestartpoint=1
+            picturename="IMG"
+            pictureformat="png"
+    return init_point_ratio, step_ratio,yfixset,xfixset,fixlinestartpoint, picturename, pictureformat
+
 def ReadInPic():
-    img = cv2.imread(PicPath)
+    global current_img_index
+    init_point_ratio, step_ratio,yfixset,xfixset,fixlinestartpoint, picturename, pictureformat = read_pic_set("readpicset.json")
+    # 拼接当前图像文件名
+    img_path = f"{picturename}{current_img_index}.{pictureformat}"
+    img = cv2.imread(img_path)
+    # 从指定路径（PicPath）读取图像并存储在img变量中
+    #img = cv2.imread(PicPath)
+    
+    # 尝试从另一个指定路径（IMGPATH）读取图像，并将其缩放为原尺寸的一半后显示在屏幕上
+    # 如果在读取第二个图像时出现异常（错误），则从函数中返回
     try:
         img2 = cv2.imread(IMGPATH)
-        img2 = cv2.resize(img2,None,fx=0.5,fy=0.5)
+        img2 = cv2.resize(img2, None, fx=0.5, fy=0.5)
         cv2.imshow("output2", img2)
     except:
-        return
+       pass
+    
+    # 检查图像"img"是否具有正确的形状（非空）
+    # 如果在访问图像形状时出现异常（错误），则从函数中返回
     try:
         img.shape
     except:
         return
-    HEIGHT,WIDTH = img.shape[:2]
-    initpoint = (int(WIDTH*0.43),int(HEIGHT*0.1))
-    k = [30, 30.75, 31.5, 32.25, 33, 33.75, 34.5, 35.25, 36]
-    k2 = np.math.tan(pi* 5 / 6)
-    deltax = WIDTH * 0.05
-    SquareColor = []
-    for i in range(0,9):
-        linestartpoint = (int(initpoint[0] - deltax * i), int(initpoint[1] - deltax * i *k2))
-        k1 = np.math.tan((k[i]-2) / 180 * pi)
-        b = linestartpoint[1] - linestartpoint[0] * k1
-        LineColor = []
-        for j in range(0,9):
-            pointx = int(linestartpoint[0] + j * deltax)
-            pointy = int(linestartpoint[1] + j * deltax * k1)
-            if(pointx>WIDTH):
-                pointx=WIDTH-1
-            if(pointy>HEIGHT):
-                pointy=HEIGHT-1
-            point = (pointx,pointy)
-            color = img[pointy,pointx]
+    
+    # 获取图像"img"的高度（HEIGHT）和宽度（WIDTH）
+    HEIGHT, WIDTH = img.shape[:2]
 
-            isBlue = (color[0] > (color[1] * 1.3)) & (color[0] > (color[2] * 1.3))
-            isGreen = (color[1] > (color[0] * 1.3)) & (color[1] > (color[2] * 1.3))
-            isRed = (color[2] > (color[0] * 1.3)) & (color[2] > (color[1] * 1.3))
-            isYellow = (color[2] > (color[0] * 1.3)) & (color[1] > (color[0] * 1.3))
-            PointColor = (0,0,0)
-            if isBlue:
-                PointColor = (255,0,0)
-                LineColor.append(1)
-            elif isRed:
-                PointColor = (0,0,255)
-                LineColor.append(2)
-            elif isGreen:
-                PointColor = (0,255,0)
-                LineColor.append(4)
-            elif isYellow:
-                PointColor = (0,255,255)
-                LineColor.append(3)
-            else:
+    
+    # 定义初始点"initpoint"，从这个点开始处理图像
+    initpoint = (int(WIDTH * init_point_ratio[0]), int(HEIGHT * init_point_ratio[1]))
+    # 计算步长"deltax"，用于沿x轴移动的大小，基于图像宽度的5%
+    deltax = WIDTH * step_ratio
+    
+    
+    
+    # 定义一个包含不同角度（以度为单位）的列表"k"
+    k = [30, 30.75, 31.5, 32.25, 33, 33.75, 34.5, 35.25, 36]
+    
+    # 计算tan(pi/6)（30度）的值，并存储在"k2"变量中
+    k2 = np.math.tan(pi * 5 / 6)
+    
+
+    
+    # 初始化一个空列表"SquareColor"，用于存储格子的颜色信息
+    SquareColor = []
+    
+    # 循环遍历9x9格子中的每一行
+    for i in range(0, 9):
+        # 计算每一行的起始点"linestartpoint"
+        linestartpoint = (int(initpoint[0] - deltax * i*fixlinestartpoint), int(initpoint[1] - deltax * i * k2*fixlinestartpoint))
+        
+        # 计算角度"k[i]-2"的tan值，并存储在"k1"变量中
+        k1 = np.math.tan((k[i]) / 180 * pi)
+        
+        # 计算直线的截距"b"，基于起始点和"k1"
+        b = linestartpoint[1] - linestartpoint[0] * k1
+        
+        # 初始化一个空列表"LineColor"，用于存储每一行格子的颜色信息
+        LineColor = []
+        
+        # 循环遍历每一行格子中的每一列
+        for j in range(0, 9):
+            # 计算每个格子的x和y坐标，基于起始点和步长"deltax"//现在为step_ratio
+            pointx = int(linestartpoint[0] + j * deltax*xfixset)
+            pointy = int(linestartpoint[1] + j * deltax * k1*yfixset)
+            
+            # 确保计算得到的坐标不超过图像的尺寸范围
+            if pointx > WIDTH or pointy > HEIGHT:
                 LineColor.append(6)
-            cv2.circle(img,point,2,PointColor,4)
+            else:
+            # 创建一个元组"point"，用于存储当前格子的(x, y)坐标
+                point = (pointx, pointy)
+            
+            # 获取当前格子在图像中的颜色
+                #color = img[pointy, pointx]
+                color = evaluate_color_around_pixel(img, pointx, pointy, 5 , 255*0.95)
+            # 检查格子的颜色，以确定格子类型，并将对应的值添加到"LineColor"列表中
+                if(color is not None):
+                    isBlue = (color[0] > (color[1] * 1.3)) & (color[0] > (color[2] * 1.3))
+                    isGreen = (color[1] > (color[0] * 1.3)) & (color[1] > (color[2] * 1.3))
+                    isRed = (color[2] > (color[0] * 1.3)) & (color[2] > (color[1] * 1.4))
+                    isYellow = (color[2] > (color[0] * 1.3)) & (color[1] > (color[0] * 1.3))
+            
+            # 根据颜色类型设置当前格子的颜色，并将对应的值添加到"LineColor"列表中
+                PointColor = (0, 0, 0)
+                if isBlue:
+                    PointColor = (255, 0, 0)
+                    LineColor.append(1)
+                elif isRed:
+                    PointColor = (0, 0, 255)
+                    LineColor.append(2)
+                elif isGreen:
+                    PointColor = (0, 255, 0)
+                    LineColor.append(4)
+                elif isYellow:
+                    PointColor = (0, 255, 255)
+                    LineColor.append(3)
+                else:
+                    LineColor.append(6)
+                
+                # 在当前格子的位置绘制一个小圆圈，使用确定的颜色进行可视化
+                cv2.circle(img, point, 2, PointColor, 4)
+        
+        # 将当前行格子的颜色信息添加到"SquareColor"列表中
         SquareColor.append(LineColor)
+    
+    # 读取"config.json"文件的内容，并将其存储在变量"Config"中
     Config = None
     with open(ConfigPath, 'r') as f:
         Config = json.load(f)
+    
+    # 使用格子的颜色信息更新"Config"中的"InitQuads"属性
     Config["InitQuads"] = SquareColor
-
+    
+    # 创建临时文件路径和最终文件路径，用于存储更新后的配置
     NewConfigPathTmp = os.getcwd() + "\\NewConfigTmp.txt"
     NewConfigPath = os.getcwd() + "\picconfig.json"
-
-    f = open(NewConfigPathTmp,'w+')
-    json.dump(Config, f, indent = 0, separators = (",",":"))
+    
+    # 将更新后的配置以JSON格式写入临时文件
+    f = open(NewConfigPathTmp, 'w+')
+    json.dump(Config, f, indent=0, separators=(",", ":"))
     f.close()
-    with open(NewConfigPathTmp,"r") as fin:
+    
+    # 将临时文件的内容复制到最终文件中，并进行适当的格式化（删除换行符）
+    with open(NewConfigPathTmp, "r") as fin:
         with open(NewConfigPath, "w+") as fout:
             for line in fin:
-                if not ("]," in line):
-                    fout.write(line.replace("\n",""))
+                if not (",]" in line):
+                    fout.write(line.replace("\n", ""))
                 else:
                     fout.write(line)
+    
+    # 删除临时文件
     os.remove(NewConfigPathTmp)
+    
+    # 将图像"img"缩放为原尺寸的一半，并使用OpenCV显示在屏幕上
     img.resize()
-    img = cv2.resize(img,None,fx=0.5,fy=0.5)
+    img = cv2.resize(img, None, fx=0.5, fy=0.5)
+    # 在initpoint位置绘制一个圆圈来高亮显示
+    scaled_initpoint = (int(initpoint[0] * 0.5), int(initpoint[1] * 0.5))
+    cv2.circle(img, scaled_initpoint, 15, (255, 255, 255), -1)
     cv2.imshow("output", img)
-   
+
+    # 定义一个函数用于切换到下一个图像
+def switch_to_next_img():
+    global current_img_index
+    current_img_index += 1
+
 def GoOneStep(screen, Position, SquareColor):
     global CurrentIndex
     global UsedColorQueue
@@ -173,10 +309,17 @@ def OutputNewConfig(Config):
                 else:
                     fout.write(line)
     os.remove(NewConfigPathTmp)
-        
+
+def configread():
+    Config = None
+    with open(ConfigPath, 'r') as f:
+        NewColorListjson = json.load(f)
+    SquareColor = NewColorListjson["InitQuads"] 
+    return NewColorListjson,SquareColor
 
 def main():
     print(cv2.__file__)
+
     ReadInPic()
 
     pygame.init()
@@ -184,13 +327,15 @@ def main():
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     pygame.display.set_caption('Alchemy star sim')
 
-    Config = None
-    with open(ConfigPath, 'r') as f:
-        NewColorListjson = json.load(f)
+    NewColorListjson,SquareColor=configread()
+    # Config = None
+    # with open(ConfigPath, 'r') as f:
+    #     NewColorListjson = json.load(f)
+    # SquareColor = NewColorListjson["InitQuads"]
     #print(Config)
     editing_mode = False
     
-    SquareColor = NewColorListjson["InitQuads"]
+    
     LastPos = []
     while True:
         screen.fill((0))
@@ -221,6 +366,12 @@ def main():
                     editing_mode = not editing_mode
                     print("editing_mode:",editing_mode)
                     print("KEYUP:",event.key)
+                elif event.key == 13:  # 按下回车键
+                    switch_to_next_img()
+                    ReadInPic()
+                    NewColorListjson,SquareColor=configread()
+                    WalkedPath.clear()
+                    #CurrentIndex=CurrentIndex+1
                 if (editing_mode == True):
                         if event.key == K_1:
                             ChangeQuadColor(LastPos,SquareColor,1)
